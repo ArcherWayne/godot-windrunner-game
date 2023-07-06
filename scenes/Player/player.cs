cd720d724e370146dae4c205d933e82b4e8a733f
Archer Wayne <archerwayne1216@gmail.com>
2023-07-06 23:48:20 +0800

    add tree script and update player script
    Signed-off-by: Archer Wayne <archerwayne1216@gmail.com>

--- scenes/Player/player.cs
+++ scenes/Player/player.cs
@@ -1,27 +1,36 @@
 using Godot;
 using System;
 
-public partial class player : Node2D
+public partial class player : CharacterBody2D
 {
-	int movementSpeed = 200;
+	int movementSpeed = 1200;
 	Vector2 playerPos = new Vector2();
+
+	public override void _Ready(){
+	}
+
+
 	public override void _Process(double delta)
 	{
 		// input
-		var direction = Input.GetVector("left", "right", "up", "down");
-
-		// Position += direction * movementSpeed * delta;
-		playerPos.X += direction.X * movementSpeed * (float)delta;
-		playerPos.Y += direction.Y * movementSpeed * (float)delta;
-
-		Position = playerPos;
+		var carNode = GetNode<Sprite2D>("../Car");
 
 		// arrow shooting input
-		if (Input.IsActionPressed("primary action")){
-			GD.Print("shoot arrow");
-
+		if (Input.IsActionJustPressed("primary action")){
+			// for the purpose of practise, access car node from this node
+			// and the primary action is to rotate the car 90 degrees clockwsie. 
+			carNode.RotationDegrees += 90;
 		}
+	}
 
+	public override void _PhysicsProcess(double delta){
+		var direction = Input.GetVector("left", "right", "up", "down");
+		// Position += direction * movementSpeed * delta;
+		// playerPos.X += direction.X * movementSpeed * (float)delta;
+		// playerPos.Y += direction.Y * movementSpeed * (float)delta;
+		Velocity = direction * movementSpeed * (float)delta;
 
+		GD.Print(Velocity);
+		MoveAndSlide();
 	}
 }
